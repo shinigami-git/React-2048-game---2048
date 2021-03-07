5d5f1e25fb980a9128362258dd3f6dc2f52de38c
shinigami-git <nitink.kmr638@gmail.com>
2021-03-07 18:29:04 +0530

    moving and score functionality added

--- src/components/Board.js
+++ src/components/Board.js
@@ -113,15 +113,23 @@ export default function Board(props) {
               board[r][c] = board[r][c] * 2;
               board[r][c - 1] = 0;
               score += board[r][c];
-            } else if (board[r][c] === 0 && board[r][c - 1] > 0) {
+            } 
+            // else if (board[r][c] === 0 && board[r][c - 1] > 0) {
+            //   board[r][c] = board[r][c - 1];
+            //   board[r][c - 1] = 0;
+            // }
+          }
+          for (let c = board[r].length - 1; c >= 0; c--) {
+             if (board[r][c] === 0 && board[r][c - 1] > 0) {
               board[r][c] = board[r][c - 1];
               board[r][c - 1] = 0;
             }
           }
+
         }
         board=placeRandomNumber(board);
         console.log(board);
-        return board;
+        return {board,score};
       }
 
       const moveLeft=(inputBoard)=> {
@@ -145,15 +153,29 @@ export default function Board(props) {
               board[r][c] = board[r][c] * 2;
               board[r][c + 1] = 0;
               score += board[r][c];
-            } else if (board[r][c] === 0 && board[r][c + 1] > 0) {
+            } 
+            // else if (board[r][c] === 0 && board[r][c + 1] > 0) {
+            //   board[r][c] = board[r][c + 1];
+            //   board[r][c + 1] = 0;
+            // }
+            
+
+          }
+
+          for (let c = 0; c < board.length; c++) {
+             if (board[r][c] === 0 && board[r][c + 1] > 0) {
               board[r][c] = board[r][c + 1];
               board[r][c + 1] = 0;
             }
+            
+
           }
+
+
         }
         board=placeRandomNumber(board);
         console.log(board);
-        return board;
+        return {board,score};
       }
       const moveUp=(inputBoard)=> {
         let rotatedRight = rotateRight(inputBoard);
@@ -177,7 +199,14 @@ export default function Board(props) {
               board[r][c] = board[r][c] * 2;
               board[r][c - 1] = 0;
               score += board[r][c];
-            } else if (board[r][c] === 0 && board[r][c - 1] > 0) {
+            } 
+            // else if (board[r][c] === 0 && board[r][c - 1] > 0) {
+            //   board[r][c] = board[r][c - 1];
+            //   board[r][c - 1] = 0;
+            // }
+          }
+          for (let c = board[r].length - 1; c >= 0; c--) {
+             if (board[r][c] === 0 && board[r][c - 1] > 0) {
               board[r][c] = board[r][c - 1];
               board[r][c - 1] = 0;
             }
@@ -189,7 +218,7 @@ export default function Board(props) {
         board=placeRandomNumber(board);
         console.log(board);
     
-        return board;
+        return {board,score};
       }
 
       const moveDown=(inputBoard)=> {
@@ -214,11 +243,21 @@ export default function Board(props) {
               board[r][c] = board[r][c] * 2;
               board[r][c + 1] = 0;
               score += board[r][c];
-            } else if (board[r][c] === 0 && board[r][c + 1] > 0) {
+            }
+            //  else if (board[r][c] === 0 && board[r][c + 1] > 0) {
+            //   board[r][c] = board[r][c + 1];
+            //   board[r][c + 1] = 0;
+            // }
+          }
+
+          for (let c = 0; c < board.length; c++) {
+             if (board[r][c] === 0 && board[r][c + 1] > 0) {
               board[r][c] = board[r][c + 1];
               board[r][c + 1] = 0;
             }
           }
+
+
         }
     
         // Rotate board back upright
@@ -226,7 +265,7 @@ export default function Board(props) {
         board=placeRandomNumber(board);
         console.log(board);
     
-        return board;
+        return {board,score};
       }
 
 
@@ -238,22 +277,30 @@ export default function Board(props) {
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
         }
-    }, [])
+    }, [gameState])
+    // useEffect(() => {
+    //     console.log(gameState);
+    // }, [gameState])
     const handleKeyDown=(event)=>{
+        event.preventDefault();
         if (event.keyCode >= 37 && event.keyCode <= 40){
             // console.log(gameState);        
             if(event.keyCode==39){
                 // console.log(gameState);
-                setGameState (moveRight(gameState));
+                setScore(score+moveRight(gameState).score);
+                setGameState (moveRight(gameState).board);
             }else if(event.keyCode==38){
                 // console.log(gameState);
-                setGameState (moveUp(gameState));
+                setScore(score+moveUp(gameState).score);
+                setGameState (moveUp(gameState).board);
             }else if(event.keyCode==40){
                 // console.log(gameState);
-                setGameState (moveDown(gameState));
+                setScore(score+moveDown(gameState).score);
+                setGameState (moveDown(gameState).board);
             }else{
                 // console.log(gameState);
-                setGameState (moveLeft(gameState));
+                setScore(score+moveLeft(gameState).score);
+                setGameState (moveLeft(gameState).board);
             }
 
             console.log("key pressed with keycode",event.keyCode);  
@@ -265,12 +312,12 @@ export default function Board(props) {
           {gameState.map((row,index)=>(
               <div className="board-row" key={index}>
                   {row.map((cell,index)=>(
-                      <div className="board-box" key={index}>{cell>0 && cell}</div>
+                      <div className={cell==0?`board-box`:`board-box board-box-high`} key={index}  >{cell>0 && cell}</div>
                   ))}
               </div>
           ))}
       </div>
-      <div className="score"><h2>Score:{score}</h2></div>
+      <div className="score"><h2>Score:- {score}</h2></div>
     </>
   );
 }
